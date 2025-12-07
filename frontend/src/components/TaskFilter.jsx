37b1212b096d87be6409927e6946abd17e03b35b
dylanpeel3 <dapeel06@gmail.com>
2025-12-07 15:43:34 -0600

    Update TaskFilter.jsx

--- frontend/src/components/TaskFilter.jsx
+++ frontend/src/components/TaskFilter.jsx
@@ -1,1 +1,17 @@
-
+export default function TaskFilter({ filter, setFilter }){
+  return(
+    <div className="task-filter">
+      <label>
+        Show:
+        <select
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          >
+            <option value="all></option>
+            <option value="todo">todo</option>
+            <option value="done">done</option>
+        </select>
+    </label>
+  </div>
+);
+}
