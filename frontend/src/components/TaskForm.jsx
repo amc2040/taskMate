dc6cdff1e696b3a54d32d316ed5f5ce6e1f5987c
dylanpeel3 <dapeel06@gmail.com>
2025-12-07 15:37:30 -0600

    Update TaskForm.jsx

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,1 +1,28 @@
+import{useState} from "react";
+export default functin TaskForm({onAddTask}){
+  const[title, setTitle] = useState("");
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(title.trim() === "") return;
+    const newTask = {
+      id:
+      title: title.trim(),
+      status: "todo",
+    };
 
+    onAddTask(newTask);
+    setTitle("");
+  };
+
+  return(
+    <form className="task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="New task..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        />
+        <button type="submit">Add</button>
+    </form>
+    );
+}
