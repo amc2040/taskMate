35b867102ae5d121190507c6d121c546efcabe52
zlbhc4 <zlbhc4@umsystem.edu>
2025-12-08 11:38:33 -0600

    updated code

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,8 +1,13 @@
-function errorHandler(err, req, res, next) {
-    console.error("Error:", err);
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);};
+  const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
     res.status(statusCode).json({
       message: err.message,
-      stack: process.env.NODE_ENV === "production" ? null : err.stack});}
-  module.exports = errorHandler;
+      stack: process.env.NODE_ENV === 'production' ? null : err.stack,});};
+  module.exports = {
+    notFound,
+    errorHandler,};
   
