2a53bde18f679a494a07de6ef7cd195dcdac9fec
dylanpeel3 <dapeel06@gmail.com>
2025-12-08 11:43:22 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,33 @@
+import {useState} from "react";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import TaskFilter from "./components/TaskFilter";
 
+export default function App(){
+    const[tasks, setTasks] = useState([]);
+    const[filter, setFilter] = useState("all")
+    const handleAddTask = (task) => {
+      setTasks((prev) => [...prev, task]);
+    };
+const handleToggleDone = (id) =>{
+  setTasks((prev) =>
+    prev.map((task) =>
+      task.id === id
+        ?{...task, status: task.status === "done" ? "todo" : "done"}
+        : task
+    )
+  );
+};
+const filteredTasks = tasks.filter((task) =>{
+  if(filter === "all") return true;
+  return task.stauts === filter;
+});
+return(
+  <div>
+    <h1>taskMate</h1>
+    <TaskForm onAddTask={handleAddTask}/>
+    <TaskFilter filter={filter} setFilter={setFilter}/>
+    <TaskList tasks={filteredTasks} onToggleDone={handdleToggleDone}/>
+  </div>
+ );
+}
