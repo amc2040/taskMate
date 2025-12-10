8f155e57501244e6e3504ed6bd967af1d2112847
amc2040 <ambercarr_04@yahoo.com>
2025-12-10 17:47:47 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const taskRoutes = require('./routes/taskRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
-dotenv.config();
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 connectDB();
