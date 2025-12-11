2cf2d341f9c0a56718c12f42f43cb78df30d7b4b
amc2040 <ambercarr_04@yahoo.com>
2025-12-10 18:02:45 -0600

    Update TaskItem.jsx

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,20 +1,18 @@
-export default function TaskItem({task, onToggleDone}){
-  const id = task.id;
+export default function TaskItem({ task, onToggleDone }) {
+  const id = task._id;   // FIXED
 
-  return(
+  return (
     <li className={`task-item ${task.status}`}>
-
       <input
         type="checkbox"
         checked={task.status === "done"}
         onChange={() => onToggleDone(id)}
       />
-      
+
       <div>
         <strong>{task.title}</strong>
         <span className="status">({task.status})</span>
       </div>
-      
     </li>
   );
 }
