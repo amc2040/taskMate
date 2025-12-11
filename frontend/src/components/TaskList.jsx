f8e2b714fce69082723d4c7d7cefaa0061d89e8d
amc2040 <ambercarr_04@yahoo.com>
2025-12-10 18:03:29 -0600

    Update TaskList.jsx

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,10 +1,15 @@
 import TaskItem from "./TaskItem";
-export default function TaskList({tasks, onToggleDone}){
-  return(
+
+export default function TaskList({ tasks, onToggleDone }) {
+  return (
     <ul className="task-list">
-      {tasks.map((task) =>(
-        <TaskItem key={task.id} task={task} onToggleDone={onToggleDone}/>
-        ))}
-      </ul>
-    );
+      {tasks.map((task) => (
+        <TaskItem
+          key={task._id}        // FIXED
+          task={task}
+          onToggleDone={onToggleDone}
+        />
+      ))}
+    </ul>
+  );
 }
