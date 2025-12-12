310c1cf46f60fab95e6cdf0a8d706f2f74def144
amc2040 <ambercarr_04@yahoo.com>
2025-12-11 22:47:10 -0600

    Update ProgressTracker.jsx

--- frontend/src/components/ProgressTracker.jsx
+++ frontend/src/components/ProgressTracker.jsx
@@ -1,15 +1,15 @@
-export default function ProgressTracker({tasks}){
+export default function ProgressTracker({ tasks }) {
   const total = tasks.length;
-  const toDo = tasks.filter((t) => t.status === "to-do").length
-  const inProgress = tasks.filter((t) => t.status === "in-progress").length
-  const done = tasks.filter((t) => t.status === "done").length
+  const toDo = tasks.filter((t) => t.status === "todo").length;
+  const inProgress = tasks.filter((t) => t.status === "in progress").length;
+  const done = tasks.filter((t) => t.status === "done").length;
 
-  return(
+  return (
     <div className="progress-tracker">
       <h2>Progress</h2>
       <p>Total tasks: {total}</p>
-      <p>To-do: {toDo}</p>
-      <p>In progress: {inProgress}</p>
+      <p>To Do: {toDo}</p>
+      <p>In Progress: {inProgress}</p>
       <p>Done: {done}</p>
     </div>
   );
