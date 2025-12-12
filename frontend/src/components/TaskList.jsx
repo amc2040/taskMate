399572a57865c989783a217619daaa7e116b189a
amc2040 <ambercarr_04@yahoo.com>
2025-12-11 22:49:25 -0600

    Update TaskList.jsx

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,13 +1,22 @@
 import TaskItem from "./TaskItem";
 
-export default function TaskList({ tasks, onToggleDone }) {
+export default function TaskList({ tasks, onToggleDone, onDelete }) {
+  if (tasks.length === 0) {
+    return (
+      <div className="no-tasks">
+        <p>No tasks to display. Add a task to get started!</p>
+      </div>
+    );
+  }
+
   return (
     <ul className="task-list">
       {tasks.map((task) => (
         <TaskItem
-          key={task._id}        // FIXED
+          key={task._id}
           task={task}
           onToggleDone={onToggleDone}
+          onDelete={onDelete}
         />
       ))}
     </ul>
